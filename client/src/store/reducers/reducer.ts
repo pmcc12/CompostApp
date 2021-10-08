1dedfd2fbbf379a59ced3d75ffbd11ee6bed542f
Miguel Carvalho <pmccarvalho12@gmail.com>
2021-10-08 17:03:19 +0100

    typescript and redux structure done

---
+++ client/src/store/reducers/reducer.ts
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { connect } from 'react-redux'
+
+// export const reducer = (props) => {
+
+// }
+
+// const mapStateToProps = (state) => ({
+    
+// })
+
+// const mapDispatchToProps = {
+    
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(reducer)
---
+++ client/src/type.d.ts
@@ -0,0 +1,20 @@
+//This file holds Typescript types. Is placed in src root in order to be accessible for used in other files without importing
+
+/*
+interface IArticle {
+  id: number
+  title: string
+  body: string
+}
+
+type ArticleState = {
+  articles: IArticle[]
+}
+
+type ArticleAction = {
+  type: string
+  article: IArticle
+}
+
+type DispatchType = (args: ArticleAction) => ArticleAction
+*/
