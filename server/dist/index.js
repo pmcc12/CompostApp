2d4c0a852a1b4a76efcecbd0f3b5d66d23efcbf0
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-10-09 17:16:38 +0100

    feat(be): basic testing & src setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,16 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
-const router_1 = __importDefault(require("./router"));
+const index_route_1 = __importDefault(require("./routes/index.route"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const app = (0, express_1.default)();
-const PORT = process.env.PORT || 5000;
 dotenv_1.default.config();
-// require("dotenv").config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)('dev'));
-app.use(router_1.default);
+app.use(index_route_1.default);
 app.listen(PORT, () => {
     /* tslint:disable-next-line */
     console.log(`🚀 Server is listening at http://localhost:${PORT}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import router from './router';
+import router from './routes/index.route';
 import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
-dotenv.config()
-// require("dotenv").config();
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'));
 app.use(router);
+
 
 app.listen(PORT, () => {
     /* tslint:disable-next-line */
