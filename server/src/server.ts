d2eb150a8acc13202c0e9c4fa474acdf85806cf0
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-10-14 17:40:36 +0100

    feat(aws-be): implement s3 upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 import morgan from 'morgan';
 import router from './routes/index.route';
 import dotenv from 'dotenv';
+import fileUpload from 'express-fileupload';
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload());
 app.use(morgan('dev'));
 app.use('/api', router);
 
---
+++ server/src/utils/aws.ts
@@ -0,0 +1,52 @@
+import { Request, } from "express";
+// AWS
+const AWS = require('aws-sdk');
+// Ts
+interface FilesRequest extends Request {
+    files: any;
+}   
+
+const s3UploadImg = async (req: Request) => {
+    AWS.config.update({
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        region: process.env.S3_BUCKET_REGION,
+    })
+
+    const s3 = new AWS.S3();
+
+    // Binary data base64
+    const fileContent = Buffer.from((req as FilesRequest).files.image.data, 'binary');
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: 'compost-app-2-image-s3-bucket',
+        Key: Date.now().toString(), // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Upload to s3
+
+    // var imgData: any;
+    // await s3.upload(params, (err: any, data: any) => {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     imgData = data
+    //     console.log(imgData)
+    // });
+
+    try {
+
+        const { Location } = await s3.upload(params).promise();
+        return Location;
+
+    } catch (error) {
+        /* tslint:disable-next-line */
+        console.log(error);
+    }
+
+    
+}
+
+export default s3UploadImg;
