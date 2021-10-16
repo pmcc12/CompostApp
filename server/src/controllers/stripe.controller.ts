2e9c23bd85f1fbc4c16548ddf5c072847c4cdad6
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-10-16 14:06:36 +0100

    feat(stripe-be): implement simple stripe endpoint

--- server/src/controllers/stripe.controller.ts
+++ server/src/controllers/stripe.controller.ts
@@ -1,19 +1,43 @@
 import { Request, Response, NextFunction } from 'express';
 import * as auth from '../services/auth.service';
 import createError from 'http-errors';
+// Stripe
+const stripe = require('stripe')(process.env.STRIPE_KEY)
 
-const register = async (req: Request, res: Response, next: NextFunction) => {
+
+
+const stripeCheckout = async (req: Request, res: Response, next: NextFunction) => {
+
+    console.log(process.env.STRIPE_KEY)
+
     try {
-        const user = await auth.register(req.body);
-        res.status(201).json({
-            status: true,
-            message: "User created successful",
-            data: user
-        })
+        const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: 'T-shirt',
+                    },
+                    unit_amount: 2000,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `http://localhost:${process.env.PORT}/payment/success`,
+        cancel_url: `http://localhost:${process.env.PORT}/payment/cancel`,
+        });
+
+        console.log('Do something')
+
+
+        res.redirect(303, session.url);
     }
     catch (e: any) {
         next(createError(e.statusCode, e.message))
     }
 }
 
-export { register, login, all };
+export { stripeCheckout };
