95c5931f0cead0985777a8c5d779047d18a1f566
Abriele Qudsi <abrielebromozentia@gmail.com>
2021-10-17 10:00:19 +0100

    feat(stripe): implement webhooks

--- server/dist/controllers/stripe.controller.js
+++ server/dist/controllers/stripe.controller.js
@@ -12,32 +12,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.stripeCheckout = void 0;
+exports.stripeWebhook = exports.stripeCheckout = void 0;
 const http_errors_1 = __importDefault(require("http-errors"));
+const client_1 = require("@prisma/client");
+// Prisma
+const prisma = new client_1.PrismaClient();
 // Stripe
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const stripeCheckout = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(process.env.STRIPE_KEY);
     try {
+        const { topUpAmount } = req.body;
+        if (!topUpAmount) {
+            throw new http_errors_1.default.NotFound("Need to provide topUpAmount in body");
+        }
         const session = yield stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
+            payment_method_types: ["card"],
             line_items: [
                 {
                     price_data: {
-                        currency: 'usd',
+                        currency: "eur",
                         product_data: {
-                            name: 'T-shirt',
+                            name: "Top Up",
                         },
-                        unit_amount: 2000,
+                        unit_amount: topUpAmount,
                     },
                     quantity: 1,
                 },
             ],
-            mode: 'payment',
-            success_url: `http://localhost:${process.env.PORT}/payment/success`,
-            cancel_url: `http://localhost:${process.env.PORT}/payment/cancel`,
+            mode: "payment",
+            success_url: `http://localhost:${process.env.CLIENT_PORT}/payment/success`,
+            cancel_url: `http://localhost:${process.env.CLIENT_PORT}/payment/cancel`,
         });
-        console.log('Do something');
+        console.log("Do something");
         // ADD BALANCE
         res.redirect(303, session.url);
     }
@@ -46,3 +52,67 @@ const stripeCheckout = (req, res, next) => __awaiter(void 0, void 0, void 0, fun
     }
 });
 exports.stripeCheckout = stripeCheckout;
+const endpointSecret = process.env.STRIPE_SIGN_SECRET;
+const stripeWebhook = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    // This is your Stripe CLI webhook secret for testing your endpoint locally.
+    //   console.log(endpointSecret);
+    try {
+        const sig = req.headers["stripe-signature"];
+        let event;
+        try {
+            event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+        }
+        catch (e) {
+            res.status(400).send(`Webhook Error: ${e.message}`);
+            return;
+        }
+        // Handle the event
+        switch (event.type) {
+            case "charge.succeeded":
+                const charge = event.data.object;
+                // console.log(charge);
+                // Find User
+                const user = yield prisma.user.findUnique({
+                    where: {
+                        email: charge.billing_details.email,
+                    },
+                });
+                // Update User
+                let userEmail;
+                if (user) {
+                    userEmail = user.email;
+                }
+                else {
+                    throw new http_errors_1.default.NotFound(`${charge.billing_details.email} email doesn't exist in database`);
+                }
+                const updatedUser = yield prisma.user.update({
+                    where: {
+                        email: userEmail,
+                    },
+                    data: {
+                        balance: {
+                            increment: charge.amount,
+                        },
+                    },
+                    select: {
+                        balance: true,
+                    },
+                });
+                // Then define and call a function to handle the event charge.succeeded
+                break;
+            // ... handle other event types
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+        }
+        // Return a 200 response to acknowledge receipt of the event
+        res.status(200).json({
+            status: true,
+            message: "Top Up successful",
+            data: null,
+        });
+    }
+    catch (e) {
+        next((0, http_errors_1.default)(e.statusCode, e.message));
+    }
+});
+exports.stripeWebhook = stripeWebhook;
